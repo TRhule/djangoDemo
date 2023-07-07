04b1005611144758cca44cea0326667ac2806bc7
仲齐奇 <zqq20000520@gmail.com>
2023-07-07 11:20:27 +0800

    endpoints models

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -2,7 +2,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
-
 from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
 
 urlpatterns = [
